664e3b8e6ded40908fac97af2ec3872456ec6a40
unknown <victoriasugden@yahoo.co.uk>
2024-02-21 14:12:45 +0000

    added questions

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What needs to be installed in your project.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How should this app be used?',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who are the contributers?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license should be applied?',
+        choices: ["MIT", "APACHE", "IBM", "NONE"],
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should run the tests?',
+        default: 'npm run test',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github url?',
+      }
 ];
 
 // function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+
+## Contributing
+
+${data.contributing}
+
+## License
+
+${data.license}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions, you can reach me at ${data.email}. You can view my work at ${data.github}
+
+
 `;
 }
 
