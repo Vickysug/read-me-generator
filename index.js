fceaa8f8c6372f0ab44182eb5a4196a384427139
unknown <victoriasugden@yahoo.co.uk>
2024-02-27 14:05:42 +0000

    added functions

--- index.js
+++ index.js
@@ -56,11 +56,14 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(),fileName),data)
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((answers)=>{
+    writeToFile("template.md", generateMarkdown({ ...answers })) 
+  })
 }
 
 // function call to initialize program
